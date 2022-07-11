d5e57c905fe3495bb67dcada6dbaed6ccdc01e49
Evgeni <666997@bk.ru>
2022-07-11 19:23:16 +0800

    Task3

---
+++ task3.py
@@ -0,0 +1,18 @@
+# Задайте список из n чисел последовательности $(1+1/n)^n$ и выведите на экран их сумму.
+#
+# Пример:
+#
+# - Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
+
+n = int(input('--> ',))
+result = []
+sum = 0
+for i in range(1, n + 1):
+    result.append((1 + 1 / i) ** i)
+for j in range(n):
+    if j < n - 1:
+        print(f'{j + 1}: {result[j]}', end= ', ')
+    else:
+        print(f'{j + 1}: {result[j]}')
+    sum += float(result[j])
+print(f'Сумма = {sum}')
